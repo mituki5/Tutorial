2e9e55f7fc767659f6f7940f4ef6fcb582e4d36f
mituki <SDT2395GP0021@edu.sca.ac.jp>
2023-06-02 12:00:17 +0900

    改造（スコア表示）

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DetectCollisions : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other) {
+    private GameObject player;
+    private PlayerController pc;
+    //[SerializeField]　int point;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        //スコアの加算？
+        player = GameObject.Find("Player");//プレイヤーを見つける
+        pc = player.GetComponent<PlayerController>();//playerControllerを保存する
+        pc.score= pc.score + 1;//Playerが持つscoerに対して加算する
+        pc.SetCountText();//Playerにスコア更新を依頼
+        //pc.SetCountText(point);
+        
         Destroy(gameObject);//アタッチされている自分自身を消す
         Destroy(other.gameObject);//ぶつかった相手（other）を消す
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,6 +9,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float xRange;
     [SerializeField] private GameObject projectilePrefab;//食べ物プレハブ（あとで複製）
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text winText;
+    public int score = 0;//スコア。ほんとはよくないけどPublicにします。
+    //[SerializeField] Text scoreText;//スコア表示用Text
+
+    private void Start()
+    {
+        SetCountText();
+        winText.text = "";
+        //SetCountText(0);//初期化
+        }
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
@@ -32,4 +44,28 @@ public class PlayerController : MonoBehaviour
         }
 
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            score = score + 1;
+            SetCountText();
+        }
+    }
+    
+    public void SetCountText()
+    {
+        scoreText.text = "Count: " + score.ToString();
+        if (score >= 6)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+    //内部的なscoreをUI更新させる
+    //public void SetCountText(int point)
+    //{
+    //    score += point;//動物からもらったpointを自分のスコアに加算する
+    //    scoreText.text = "Score" + score.ToString();//【重要】UI更新
+    //}
 }
