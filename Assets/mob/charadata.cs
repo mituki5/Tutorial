1cd56718ba8807d6d225bbf3d5d4e50f41ba9052
mituki <SDT2395GP0021@edu.sca.ac.jp>
2023-06-02 12:23:41 +0900

    NAME・HP書いた

---
+++ Assets/mob/charadata.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Create StusData")]
+public class charadata : ScriptableObject
+{
+    public string NAME;//キャラ・敵名
+    public int MAXHP;//最大HP
+}
